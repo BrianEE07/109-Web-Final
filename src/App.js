5a8f2286211c43ad68fd9280ce994eea1eb5399e
ray921921 <71705367+ray921921@users.noreply.github.com>
2020-12-12 21:49:30 +0800

    front end update 12/12 0949

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ const container_swi = (mode) => {
     case 'play':
       return(
         <>
-          <div class="moniter">
-            <div class="moni_name">QQㄋㄟㄋㄟ咩噗怪</div>
-            <div class="moni_status">
-              <img class="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/833/833472.svg"/>
-              <img class="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
+          <div className="moniter">
+            <div className="moni_name">QQㄋㄟㄋㄟ咩噗怪</div>
+            <div className="moni_status">
+              <img className="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/833/833472.svg"/>
+              <img className="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
             </div>
-            <div class="moni_status">
-              <img class="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/3721/3721710.svg"/>
-              <img class="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
+            <div className="moni_status">
+              <img className="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/3721/3721710.svg"/>
+              <img className="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
             </div>
-            <div class="moni_status">
-              <img class="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/83/83410.svg"/>
-              <img class="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
+            <div className="moni_status">
+              <img className="moni_status_icon" src="https://www.flaticon.com/svg/static/icons/svg/83/83410.svg"/>
+              <img className="moni_status_bar" src="https://www.flaticon.com/svg/static/icons/svg/40/40471.svg"/>
             </div>
-            <img class="moni_setting" src="https://www.flaticon.com/svg/static/icons/svg/1242/1242392.svg"/>
+            <img className="moni_setting" src="https://www.flaticon.com/svg/static/icons/svg/1242/1242392.svg"/>
           </div>
-          <div class="visual_block">
-            <div class="house">house</div>
-            <div class="interaction">
+          <div className="visual_block">
+            <div className="house">house</div>
+            <div className="interaction">
               {interaction_swi(x)}
             </div>
           </div>
@@ -38,23 +38,26 @@ const container_swi = (mode) => {
     case 'setting':
       return(
         <>
-          <div class="set_data">
-            <div class="set_property">
-              <img class="set_user_icon" src="https://www.flaticon.com/svg/static/icons/svg/64/64572.svg"/>
-              <span class="set_user_word">user001</span>
-              <img class="set_property_icon" src="https://www.flaticon.com/svg/static/icons/svg/3039/3039367.svg"/>
-              <ul class="set_property_list"></ul>
+          <div className="set_data">
+            <div className="set_property">
+              <img className="set_user_icon" src="https://www.flaticon.com/svg/static/icons/svg/64/64572.svg"/>
+              <span className="set_user_word">user001</span>
+              <img className="set_property_icon" src="https://www.flaticon.com/svg/static/icons/svg/3039/3039367.svg"/>
+              <ul className="set_property_list"></ul>
             </div>
-            <div class="set_pet">
-              <img class="set_pet_pic" src={meow}/>
-              <span class="set_pet_name">QQㄋㄟㄋㄟ咩噗怪</span>
+            <div className="set_pet">
+              <img className="set_pet_pic" src={meow}/>
+              <span className="set_pet_name">QQㄋㄟㄋㄟ咩噗怪</span>
             </div>
           </div>
-          <div class="set_mute">
-            <img class="set_mute_icon" src="https://www.flaticon.com/svg/static/icons/svg/727/727240.svg"/>
-            <input class="set_mute_button" type="checkbox"/>
+          <div className="set_mute">
+            <img className="set_mute_icon" src="https://www.flaticon.com/svg/static/icons/svg/727/727240.svg"/>
+            <input className="set_mute_button" type="checkbox"/>
           </div>
-          <button class="set_logout">Log out</button>
+          <div className="set_button_area">
+            <button className="set_logout">Log out</button>
+            <button className="set_back">back</button>
+          </div>
         </>
       )
   }
@@ -65,46 +68,46 @@ const interaction_swi = (inter) => {
     case 'menu':
       return(
         <>
-          <img class="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/3720/3720844.svg"/>
-          <img class="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/2855/2855450.svg"/>
-          <img class="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/808/808439.svg"/>
+          <img className="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/3720/3720844.svg"/>
+          <img className="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/2855/2855450.svg"/>
+          <img className="int_icon" src="https://www.flaticon.com/svg/static/icons/svg/808/808439.svg"/>
         </>
       )
     case 'food':
       return(
         <>
-          <div class="int_choose">food1</div>
-          <div class="int_choose">food2</div>
-          <div class="int_choose">food3</div>
-          <div class="int_choose">food4</div>
-          <div class="int_choose">back</div>
+          <div className="int_choose">food1</div>
+          <div className="int_choose">food2</div>
+          <div className="int_choose">food3</div>
+          <div className="int_choose">food4</div>
+          <div className="int_choose">back</div>
         </>
       )
     case 'bath':
       return(
         <>
-          <div class="int_choose">bath1</div>
-          <div class="int_choose">bath2</div>
-          <div class="int_choose">bath3</div>
-          <div class="int_choose">bath4</div>
-          <div class="int_choose">back</div>
+          <div className="int_choose">bath1</div>
+          <div className="int_choose">bath2</div>
+          <div className="int_choose">bath3</div>
+          <div className="int_choose">bath4</div>
+          <div className="int_choose">back</div>
         </>
       )
     case 'play':
       return(
         <>
-          <div class="int_choose">play1</div>
-          <div class="int_choose">play2</div>
-          <div class="int_choose">play3</div>
-          <div class="int_choose">play4</div>
-          <div class="int_choose">back</div>
+          <div className="int_choose">play1</div>
+          <div className="int_choose">play2</div>
+          <div className="int_choose">play3</div>
+          <div className="int_choose">play4</div>
+          <div className="int_choose">back</div>
         </>
       )
   }
 }
 function App() {
   return (
-    <div class="container">
+    <div className="container">
       {container_swi(mode)}
     </div>
   )

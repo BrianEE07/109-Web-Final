c48587e74f3028f7c79cd679df74cea66dfd747d
ray921921 <71705367+ray921921@users.noreply.github.com>
2021-01-17 18:10:02 +0800

    1/17arr

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Login from "./login/login"
+import App from "./App"
 import Game from "./game"
-
+import Setting from "./setting"
 import SignUp from "./login/signup";
 
 
@@ -13,6 +14,7 @@ import SignUp from "./login/signup";
    <BrowserRouter>
     <Switch>
 
+     <Route exact path="/" component={App} />
      <Route path="/login" component={Login} />
      <Route path="/signup" component={SignUp} />
      <Route path="/game" component={Game} />

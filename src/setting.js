c2693b619fddddaebc388a3fdfe304167731d2d0
ray921921 <71705367+ray921921@users.noreply.github.com>
2021-01-19 10:12:26 +0800

    AHH1012

--- src/setting.js
+++ src/setting.js
@@ -1,39 +1,58 @@
-import './App.css'
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
+import './setting.css'
+import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import React, { useEffect, useRef, useState } from 'react'
-import meow from "./img/ahhmeow.jpg"
+import React, { useEffect, useRef, useState } from 'react';
+import chicken from "./img/setting/icebird3.png";
+import Button from '@material-ui/core/Button';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 function Setting() {
+    const[chickenName, setChickenName] = useState("冰鳥還敢下來阿");
     return (
-      <div className="container">
-        <div className="set_data">
-            <div className="set_property">
+      <Grid className="container_bg">
+      <Grid className="container_set">
+        <Grid className="set_data">
+            <Grid className="set_property">
               <img className="set_user_icon" src="https://www.flaticon.com/svg/static/icons/svg/64/64572.svg"/>
-              <span className="set_user_word">user001</span>
+              <Grid className="set_user_word"></Grid>
               <img className="set_property_icon" src="https://www.flaticon.com/svg/static/icons/svg/3039/3039367.svg"/>
-              <ul className="set_property_list"></ul>
-            </div>
-            <div className="set_pet">
-              <img className="set_pet_pic" src={meow}/>
-              <span className="set_pet_name">QQㄋㄟㄋㄟ咩噗怪</span>
-            </div>
-          </div>
-          <div className="set_mute">
+              <ul className="set_property_list">
+                <Grid>opopopopo</Grid>
+              </ul>
+            </Grid>
+            <Grid className="set_pet">
+              <img className="set_pet_pic" src={chicken}/>
+              <Grid className="set_pet_name">{chickenName}</Grid>
+            </Grid>
+          </Grid>
+          <Grid className="set_mute">
             <img className="set_mute_icon" src="https://www.flaticon.com/svg/static/icons/svg/727/727240.svg"/>
             <input className="set_mute_button" type="checkbox"/>
-          </div>
-          <div className="set_button_area">
-            <Link to="/">
-              <button classNameName="set_logout">Log out</button>
-            </Link>
+          </Grid>
+          <Grid className="set_button_area">
             <Link to="/game">
-                <button className="set_back">back</button>
+            <Button
+                variant="contained"
+                color="primary"
+                startIcon={<ArrowBackIcon />}
+                className="set_back_button"
+              >
+                Back
+              </Button>
             </Link>
-          </div>
-      </div>
+            <Link to="/">
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<ExitToAppIcon />}
+              >
+                Log_out
+              </Button>
+            </Link>
+          </Grid>
+      </Grid>.
+      </Grid>
     )
   }
   
